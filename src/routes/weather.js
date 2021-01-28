33b5b4e6c8fa6c4fa3e042a352b2ccccc6bdaa54
TengbinHuang <52409477+TengbinHuang@users.noreply.github.com>
2021-01-28 22:45:35 +1100

    Add files via upload

---
+++ src/routes/weather.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const axios = require('axios');
+
+const router = express.Router();
+const APPID = process.env.APPID;
+
+router.get('/:cc/:city', (req, res) => {
+    // res.send('weather');
+    const {
+        cc,
+        city
+    } = req.params;
+
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${cc}&APPID=${APPID}`)
+        .then(response => {
+            res.send(response.data);
+        })
+        .catch(err => console.log(err));
+});
+
+module.exports = router;
